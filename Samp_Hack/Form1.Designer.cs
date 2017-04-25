b9210007f207c7e9bf3388542bd9741f5cdea779
Comraden <Null>
2017-04-25 20:58:18 +0800

    V0.1

---
+++ Samp_Hack/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Samp_Hack
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.updateHealth = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 227);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Inject";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(12, 204);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(59, 17);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "Gravity";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(12, 158);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(90, 17);
+            this.checkBox3.TabIndex = 4;
+            this.checkBox3.Text = "Unlinited Run";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(12, 181);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(64, 17);
+            this.checkBox4.TabIndex = 5;
+            this.checkBox4.Text = "Invisible";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // updateHealth
+            // 
+            this.updateHealth.Tick += new System.EventHandler(this.updateHealth_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Health=";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "0";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 25);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(165, 22);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Heal";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(187, 262);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Timer updateHealth;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Samp_Hack/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Samp_Hack
+{
+    public partial class Form1 : Form
+    {
+        static VAMemory vam;
+        static int aPlayer = 0x00B6F5F0;
+        static int aUnlimRun = 0x00B7CEE4;
+        static int aGravity = 0x00863984;
+        static int oHealth = 0x00000540;
+        static int oInvisible = 0x00000474;
+        static int aInvisible;
+        static int aHealth;
+
+        public Form1()
+        {
+            InitializeComponent();
+            try 
+            {
+                vam = new VAMemory("gta_sa");
+                aInvisible = vam.ReadInt32((IntPtr)aPlayer) + oInvisible;
+                aHealth = vam.ReadInt32((IntPtr)aPlayer) + oHealth;
+            }
+            catch { MessageBox.Show("Process not running! Please restart the game"); }
+            updateHealth.Enabled = true;
+        }
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SetGravity(checkBox2.Checked);
+            UnlimRun(checkBox3.Checked);
+            SetInvisible(checkBox4.Checked);
+        }
+        private void Heal(bool flag) 
+        {
+            if(flag == true)
+            {
+                vam.WriteFloat((IntPtr)aHealth, 100f);
+            }
+        }
+        private void SetGravity(bool flag) 
+        {
+            if (flag == true)
+            {vam.WriteFloat((IntPtr)aGravity, 0.004f);}
+            else
+            {vam.WriteFloat((IntPtr)aGravity,0.008f);}
+        }
+        private void UnlimRun(bool flag) 
+        {
+            if (flag == true){vam.WriteByte((IntPtr)aUnlimRun, 1);}
+            else
+            {vam.WriteByte((IntPtr)aUnlimRun, 0);}
+        }
+        private void SetInvisible(bool flag) 
+        {
+            if (flag == true)
+            { vam.WriteByte((IntPtr)aInvisible, 2); }
+            else
+            { vam.WriteByte((IntPtr)aInvisible, 1); }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Heal(true);
+        }
+
+        private void updateHealth_Tick(object sender, EventArgs e)
+        {
+            float health = vam.ReadFloat((IntPtr)aHealth);
+            label2.Text = Convert.ToString(health);
+        }
+    }
+}
+//Convert.ToDouble(textBox1.Text)
